65e4a3f9d41b76d981a5195dd5acb1cd639416af
Konstantinos Niktas <kniktas@palantir.com>
2015-07-22 13:49:07 -0700

    Add a few basic options

--- src/main/java/kostasniktas/one/cli/OneCli.java
+++ src/main/java/kostasniktas/one/cli/OneCli.java
@@ -3,6 +3,13 @@ package kostasniktas.one.cli;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.opennebula.client.Client;
 import org.opennebula.client.OneResponse;
 import org.opennebula.client.vm.VirtualMachine;
@@ -31,6 +38,30 @@ public class OneCli {
             System.exit(1);
         }
         
+        String searchName = null;
+        
+        Options options = new Options();
+        
+        OptionGroup optionGroupSearch = new OptionGroup();
+        optionGroupSearch.addOption(new Option(null, "search-name", true, "Search for ONE nodes by name"));
+        optionGroupSearch.setRequired(true);
+        options.addOptionGroup(optionGroupSearch);
+        
+        Option optionView = new Option("v", "view", true, "The information to display for information found");
+        options.addOption(optionView);
+        
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine commandLine = parser.parse(options, args);
+            if (commandLine.hasOption("search-name")) {
+                searchName = commandLine.getOptionValue("search-name");
+            }
+
+        } catch (ParseException exp) {
+            System.err.println("Parsing failed. Reason: " + exp.getMessage());
+            System.exit(1);
+        }
+        
         Client oneClient;
         try {
             oneClient = new Client();
@@ -43,14 +74,11 @@ public class OneCli {
                 System.exit(1);
             }
             
-            
             for (int i = 0; i < pool.getLength(); i++) {
                 VirtualMachine vm = (VirtualMachine) pool.item(i);
-                //vm.info();
-                //System.err.println(vm.getId() + "\t" + vm.getName());
-                if (vm.getName().equals("somenode")) {
+                if (vm.getName().equals(searchName)) {
                     OneResponse monitor = vm.monitoring();
-                    System.err.println("IP: " + getIPFromXML(monitor.getMessage()));
+                    System.err.println(vm.getName() + "\t" + getIPFromXML(monitor.getMessage()));
                 }
             }
         } catch (Exception e) {
